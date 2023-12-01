e58892048ad8951b8f13c751489b4651b59055a8
GuiZimmermann <zimmerma83@gmail.com>
2023-11-30 21:27:28 -0400

    seven commit

--- Application.java
+++ Application.java
@@ -1,7 +1,5 @@
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -15,27 +13,26 @@ public class Application {
         // Curtir post
         // Compartilhar post
         // Não tenho interesse / tenho interesse
-        
-        Map<Integer, Post> postList = new HashMap<>();
-        List<Integer> postOrder = new ArrayList<>();
+
+        Map<Integer, Post> postMap = new LinkedHashMap<>();
         boolean loop = true;
         String escolha;
         int id;
-        int primeiroPost = 0;
-        
+        int posicaoTela = 0;
+
         System.out.println("Bem vindo ao Instagram de pobre!");
         do {
             System.out.println("Digite (1) Para inserir um post: ");
             System.out.println("Digite (2) para curtir algum post: ");
             System.out.println("Digite (3) para compartilhar algum post: ");
             System.out.println("Digite (4) para configurar os seus interesses");
-            System.out.println("Digite (5) para listar os posts: ");
+            System.out.println("Digite (5) para listar todos os posts: ");
             System.out.println("Digite (6) para descer o feed: ");
-            System.out.println("Digite (7) para subir o feed: ");
+            System.out.println("Digite (5) para subir o feed: ");
             System.out.println("Digite (0) para encerrar o programa: ");
             System.out.print("Digite aqui a sua escolha: ");
             escolha = sc.nextLine();
-            
+
             switch (escolha) {
                 case "1":
                         System.out.print("Qual é o seu nome? ");
@@ -43,37 +40,36 @@ public class Application {
                         System.out.print("Insira o conteúdo do seu post: ");
                         String conteudo = sc.nextLine();
                         Post post = new Post(nome, conteudo, new Date(), false, false, false);
-                        postOrder.add(post.getId());
-                        postList.put(post.getId(), post);
-                        break;
-                        
-                        case "2": 
-                        System.out.print("Insira o id do post que você deseja curtir ou remover a curtida: ");
-                        id = sc.nextInt();
-                        if (postList.containsKey(id)) {
-                            if (postList.get(id).getCurtidas().equals(false)) {   
-                            postList.get(id).setCurtidas(true);
+                        postMap.put(post.getId(), post);
+                    break;
+
+                case "2": 
+                    System.out.print("Insira o id do post que você deseja curtir ou remover a curtida: ");
+                    id = sc.nextInt();
+                    if (postMap.containsKey(id)) {
+                        if (postMap.get(id).getCurtidas().equals(false)) {   
+                            postMap.get(id).setCurtidas(true);
                             System.out.println("Post curtido com sucesso!");
                         } else {
-                            postList.get(id).setCurtidas(false);
+                            postMap.get(id).setCurtidas(false);
                             System.out.println("Curtida removida!");
                         }
                     } else {
-                        System.out.println("Esse post não existe!");
+                       System.out.println("Esse post não existe!");
                     }
                     System.out.println();
                     sc.nextLine();
                     break;
-                    
-                    case "3":
+
+                case "3":
                     System.out.print("Insira o id do post que você deseja compartilhar ou remover o compartilhamento: ");
                     id = sc.nextInt();
-                    if (postList.containsKey(id)) {
-                        if (postList.get(id).getCompartilhar().equals(false)) {
-                            postList.get(id).setCompartilhar(true);
+                    if (postMap.containsKey(id)) {
+                        if (postMap.get(id).getCompartilhar().equals(false)) {
+                            postMap.get(id).setCompartilhar(true);
                             System.out.println("Post compartilhado com sucesso!");
                         } else {
-                            postList.get(id).setCompartilhar(false);
+                            postMap.get(id).setCompartilhar(false);
                             System.out.println("Compartilhamento removido!");
                         }
                     } else {
@@ -82,13 +78,13 @@ public class Application {
                     System.out.println();
                     sc.nextLine();
                     break;
-                    
-                    case "4":
+
+                case "4":
                     System.out.print("Insira o id do post que você deseja adicionar ou remover o interesse: ");
                     id = sc.nextInt();
-                    if (postList.containsKey(id)) {
-                        if (postList.get(id).getInteresse().equals(false)) {
-                            postList.get(id).setInteresse(true);
+                    if (postMap.containsKey(id)) {
+                        if (postMap.get(id).getInteresse().equals(false)) {
+                            postMap.get(id).setInteresse(true);
                             System.out.println("Interesse adicionado!");
                         } else {
                             System.out.println("Interesse removido!");
@@ -99,28 +95,29 @@ public class Application {
                     System.out.println();
                     sc.nextLine();
                     break;
-                    
-                    case "5":
-                    for (int i = primeiroPost; i < Math.min(primeiroPost + 3, postOrder.size()); i++) {
-                        System.out.println(postList.get(postOrder.get(i)));
+
+                case "5":
+                    int count = 0;
+                    for (Integer i : postMap.keySet()) {
+                        if (count >= posicaoTela && count < posicaoTela + 3) {
+                            System.out.println(postMap.get(i));
+                        }
+                        count++;
                     }
-                    System.out.println();
                     break;
-                
-                    case "6":
-                    if (primeiroPost + 3 < postOrder.size()) {
-                        primeiroPost += 3;
+    
+                case "6":
+                    if (posicaoTela + 3 < postMap.size()) {
+                        posicaoTela += 3;
                     }
-                    System.out.println();
                     break;
     
                 case "7":
-                    if (primeiroPost - 3 >= 0) {
-                        primeiroPost -= 3;
+                    if (posicaoTela - 3 >= 0) {
+                        posicaoTela -= 3;
                     }
-                    System.out.println();
                     break;
-
+                    
                 case "0":
                     loop = false;
                     break;
